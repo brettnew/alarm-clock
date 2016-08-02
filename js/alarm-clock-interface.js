60ae785ce5dbc965e3aafa4efa4af783f42e930e
Brett New and Kevin VanEvery <brett+kevin@>
2016-08-02 16:32:48 -0700

    add snooze feature

--- js/alarm-clock-interface.js
+++ js/alarm-clock-interface.js
@@ -1,18 +1,17 @@
 var Alarm = require('./../js/alarm-clock.js').alarmModule;
 
-
-
-// var intervalId = window.setInterval(alarm.checkAlarm, 1000);
-// var interval2Id = window.setInterval(alarm.currentTime, 1000);
-
 $(document).ready(function(){
   var alarm = new Alarm();
   alarm.currentTime();
-  $('#set-alarm').submit(function(event) {
+  $('#set-time').click(function() {
     alarm.alarmTime = $("input#alarm-time").val();
-    event.preventDefault();
     console.log(alarm.alarmTime);
     $('#userAlarm').text(alarm.alarmTime);
     alarm.checkAlarm();
+  });
+
+  $('#snooze-five').click(function(){
+    alarm.snoozeFive();
+    $('#userAlarm').text(alarm.alarmTime);
   });
 });
--- js/alarm-clock.js
+++ js/alarm-clock.js
@@ -1,11 +1,12 @@
 function Alarm() {
-  this.time = moment().format("HH:mm");
+  this.time = moment().format("HH:mm")
+  this.alarmSound = new Audio('audio/alarm.wav');
+;
 }
 
 Alarm.prototype.checkAlarm = function() {
   if (this.time === this.alarmTime) {
-    var alarmSound = new Audio('audio/alarm.wav');
-    alarmSound.play();
+    this.alarmSound.play();
     setInterval(this.currentTime, 1000);
   }
 };
@@ -14,6 +15,12 @@ Alarm.prototype.currentTime = function() {
   this.time = moment().format("HH:mm");
   $('#time').text(this.time);
   setInterval(this.currentTime, 1000);
+};
+
+Alarm.prototype.snoozeFive = function() {
+  this.alarmSound.pause();
+  this.alarmTime += 5;
+  this.checkAlarm();
 };
 
 exports.alarmModule = Alarm;
